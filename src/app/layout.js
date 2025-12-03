263a00298dec45a471177d6ca77829953ccf6fad
ZHOON <zhoon95@gmail.com>
2025-12-04 08:20:31 +0900

    Fix : Dependency

--- src/app/layout.js
+++ src/app/layout.js
@@ -9,7 +9,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+      <body>
         <Nav />
         {children}
       </body>
